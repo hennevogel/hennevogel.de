7450c9998e2aa61adb6722ff944fdb390c45317a
Henne Vogelsang <hvogel@opensuse.org>
2016-02-24 01:12:42 +0100

    Get a basic deploy script going with mina

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+gem 'mina'
+
 group :jekyll_plugins do
   gem 'bootstrap-sass'
   gem 'jekyll-archives'
---
+++ config/deploy.rb
@@ -0,0 +1,32 @@
+require 'mina/git'
+require 'mina/bundler'
+
+set :domain, 'hennevogel.de'
+set :deploy_to, '/srv/www/vhosts/hennevogel.de/landing'
+set :repository, 'https://github.com/hennevogel/hennevogel.de.git'
+set :branch, 'master'
+set :user, 'hvogel'
+
+# This task is the environment that is loaded for most commands, such as
+# `mina deploy` or `mina rake`.
+task :environment do
+end
+
+# Put any custom mkdir's in here for when `mina setup` is ran.
+task :setup => :environment do
+end
+
+task :build do
+  queue 'bundle exec jekyll build'
+end
+
+desc "Deploys the current version to the server."
+task :deploy => :environment do
+  deploy do
+    # Put things that will set up an empty directory into a fully set-up
+    # instance of your project.
+    invoke :'git:clone'
+    invoke :'bundle:install'
+    invoke :build
+  end
+end
